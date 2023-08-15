d2b4f066a3ac7bb6a5538b463c6155d05c8c97cd
Luna <luna@MacBook-Pro.local>
2023-08-15 20:33:53 +0100

    finished

--- wordpress/script.sh
+++ wordpress/script.sh
@@ -9,20 +9,14 @@ DIR="/var/www/html/";
 
 # downloads the WP-CLI PHAR (PHP Archive) file from the GitHub repository. The -O flag tells curl to save the file with the same name as it has on the server.
 cp -r ./wordpress/* $DIR
-chmod -R 777 /var/www/html
+chmod -R 777 $DIR
 chown -R www-data:www-data /var/www/html
 
 sed -i 's/database_name_here/'$DBNAME'/g' /var/www/html/wp-config.php
 sed -i 's/username_here/'$DBUSER'/g'    /var/www/html/wp-config.php
 sed -i 's/password_here/'$DBPASS'/g'  /var/www/html/wp-config.php
-mv www.conf /etc/php/7.3/fpm/pool.d/www.conf
+cp www.conf /etc/php/7.3/fpm/pool.d/www.conf
 service php7.3-fpm start
 service php7.3-fpm stop
 
 php-fpm7.3 -F -R
-
-
-
-
-
-# starts the PHP-FPM service in the foreground. The -F flag tells PHP-FPM to run in the foreground, rather than as a daemon in the background.
--- wordpress/wordpress/wp-config.php
+++ wordpress/wordpress/wp-config.php
@@ -20,13 +20,13 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'inception' );
+define( 'DB_NAME', 'database_name_here' );
 
 /** Database username */
-define( 'DB_USER', 'aheddak' );
+define( 'DB_USER', 'username_here' );
 
 /** Database password */
-define( 'DB_PASSWORD', 'aya2019' );
+define( 'DB_PASSWORD', 'password_here' );
 
 /** Database hostname */
 define( 'DB_HOST', 'mariadb' );
